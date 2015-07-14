13c629cb07662e1307010c53e3b0b11b4881a3ea
Blaise <bbkoch@ualr.edu>
2015-07-14 14:13:19 -0500

    Provided unoptimized implementation details for BlobProcessor.  Right now it only does motion detection but soon it will do blob detection on the moving blobs.  I suspect that using the result image as a mask for the input image will give more useful results (full color, moving objects only).  These objects can be used as stored Features in a FeatureContainer as parameter tuning for the skin tracking method aswell.

--- BlobProcessor.cpp
+++ BlobProcessor.cpp
@@ -5,7 +5,26 @@ BlobProcessor::BlobProcessor() {
 }
 
 std::shared_ptr<Frame> BlobProcessor::run(std::shared_ptr<Frame> frame) {
+	if(previous.get() == nullptr) {
+		previous = frame;
+		return std::shared_ptr<Frame>(); //return nullptrn 
+	}
 
+	if(current.get() == nullptr) {
+		current = frame;
+		return std::shared_ptr<Frame>(); //return nullptr
+	}
 
-	return std::shared_ptr<Frame>(); //return nullptr
+	cv::Mat tmp1, tmp2, result;
+
+	cv::absdiff(previous->getData(),frame->getData(),tmp1);
+	cv::absdiff(current->getData(),frame->getData(),tmp2);
+	cv::bitwise_and(tmp1,tmp2,result);
+
+	cv::threshold(result,result,35,255,CV_THRESH_BINARY);
+
+	previous = current;
+	current = frame;
+
+	return std::make_shared<Frame>(result, frame->getCameraID(), frame->getID());
 }
