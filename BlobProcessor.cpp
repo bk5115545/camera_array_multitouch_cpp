6b28da8952036e7960a3a3d878a6ed3d55596701
Blaise <bbkoch@ualr.edu>
2015-07-14 14:07:43 -0500

    Added implementation of BlobProcessor.

---
+++ BlobProcessor.cpp
@@ -0,0 +1,11 @@
+#include "BlobProcessor.h"
+
+BlobProcessor::BlobProcessor() {
+
+}
+
+std::shared_ptr<Frame> BlobProcessor::run(std::shared_ptr<Frame> frame) {
+
+
+	return std::shared_ptr<Frame>(); //return nullptr
+}
