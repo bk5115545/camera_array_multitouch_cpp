c525783b3403705eb461097ddb5e164cf99686cc
Alex Jaeger <agjaeger@ualr.edu>
2015-07-28 15:56:48 -0500

    Added Bryan's map
    The multithreading problems still persist

--- include/CalibrationProcessor.h
+++ include/CalibrationProcessor.h
@@ -19,7 +19,18 @@ private:
 	std::shared_ptr<Frame> calibratePosition(std::shared_ptr<Frame> frame);
 	void calibrateCameraValue(int camera_id, int opencv_param_id, double opencv_param_value);
 
+	bool hasElement(int index) {
+		try {
+			cache.at(index);
+			return true;
+		}
+		catch (std::out_of_range e) { return false; }
+	}
+
 public:
 	std::shared_ptr<Frame> run(std::shared_ptr<Frame> f);
+
+	typedef std::pair<std::shared_ptr<Frame>, std::shared_ptr<Frame>> __previous_current__;
+	static std::map<int, __previous_current__> cache; //i want this to be std::uniq_ptr<__previous_current__> but error
 
 };
--- src/CalibrationProcessor.cpp
+++ src/CalibrationProcessor.cpp
@@ -3,9 +3,33 @@
 
 #include "CalibrationProcessor.h"
 
+std::map<int, CalibrationProcessor::__previous_current__> CalibrationProcessor::cache = std::map<int, __previous_current__>();
+
 std::shared_ptr<Frame> CalibrationProcessor::run(std::shared_ptr<Frame> frame) {
+
+	if (!hasElement(frame->getCameraID())) {
+		__previous_current__ pair;
+		try {
+			pair = cache.at(frame->getCameraID());
+		}
+		catch (std::out_of_range e) {
+			pair = std::pair<std::shared_ptr<Frame>, std::shared_ptr<Frame>>();
+			pair.first = frame;
+			cache[frame->getCameraID()] = pair;
+
+			return std::shared_ptr<Frame>(); //return nullptr
+		}
+
+
+	}
+
+	if (cache[frame->getCameraID()].second.get() == nullptr) {
+		cache[frame->getCameraID()].second = frame;
+		return std::shared_ptr<Frame>(); //return nullptr
+	}
+
 	std::cout << "Camera ID: " << frame->getCameraID() << "\n";
-	return frame; //calibratePosition(frame);
+	return calibratePosition(frame);
 }
 
 /*
@@ -19,17 +43,14 @@ void CalibrationProcessor::calibrateLens(std::shared_ptr<Frame> frame) {
 	calibratePosition
 */
 std::shared_ptr<Frame> CalibrationProcessor::calibratePosition(std::shared_ptr<Frame> frame) {
-	int camera_id = frame->getCameraID();
-	//std::shared_ptr<CameraDevice> camera = CameraDevice::getCameraDevice(camera_id);
-
 	std::vector<cv::KeyPoint> keypoints;
-
 	cv::Mat output;
 
 	sift_detector.detect(frame->getData(), keypoints);
 	cv::drawKeypoints(frame->getData(), keypoints, output);
 
 	std::cout << frame->getCameraID() << "\n";
+
 	return std::make_shared<Frame>(output, frame->getCameraID(), frame->getID());
 }
 
