5749989e46895c5ead7c869f0e42b15459ebdc79
Alex Jaeger <agjaeger@ualr.edu>
2015-08-04 13:45:53 -0500

    Created a basic filter
    Limits the features to a maximum of 10 (for testing)

--- include/CalibrationProcessor.h
+++ include/CalibrationProcessor.h
@@ -2,7 +2,7 @@
 
 #include <memory>
 #include <atomic>
-#include <deque>
+#include <queue>
 
 #include <opencv2/nonfree/features2d.hpp>
 
@@ -10,27 +10,22 @@
 #include "Processor.h"
 
 class CalibrationProcessor : Processor {
-
+	std::shared_ptr<Frame> frame;
 	std::deque<CameraDevice> camera_locations;
+	
 	std::shared_ptr<cv::FeatureDetector> sift_detector;
+	std::vector<cv::KeyPoint> historic_features;
 
 private:
-	void calibrateLens(std::shared_ptr<Frame> frame);
-	std::shared_ptr<Frame> calibratePosition(std::shared_ptr<Frame> frame);
-	void calibrateCameraValue(int camera_id, int opencv_param_id, double opencv_param_value);
+	// Calibration Functions
+	void calibrateLens();
+	void calibratePosition();
 
-	bool hasElement(int index) {
-		try {
-			cache.at(index);
-			return true;
-		}
-		catch (std::out_of_range e) { return false; }
-	}
+	// Helpers
+	std::vector<cv::KeyPoint> getKeypoints();
+	void filterKeypoints(std::vector<cv::KeyPoint> kp);
 
 public:
 	std::shared_ptr<Frame> run(std::shared_ptr<Frame> f);
-
-	typedef std::pair<std::shared_ptr<Frame>, std::shared_ptr<Frame>> __previous_current__;
-	static std::map<int, __previous_current__> cache; //i want this to be std::uniq_ptr<__previous_current__> but error
 
 };
--- src/CalibrationProcessor.cpp
+++ src/CalibrationProcessor.cpp
@@ -1,50 +1,76 @@
 
 #include <iostream>
 #include <chrono>
+#include <algorithm>
 
 #include "CalibrationProcessor.h"
 
-std::map<int, CalibrationProcessor::__previous_current__> CalibrationProcessor::cache = std::map<int, __previous_current__>();
+std::shared_ptr<Frame> CalibrationProcessor::run(std::shared_ptr<Frame> f) {
+	frame = f;
 
-std::shared_ptr<Frame> CalibrationProcessor::run(std::shared_ptr<Frame> frame) {
-	sift_detector = std::make_shared<cv::SiftFeatureDetector>();
-	
-	auto t1 = std::chrono::high_resolution_clock::now();	
+	sift_detector = std::make_shared<cv::SiftFeatureDetector>(
+		0,		// nFeatures
+		4,		// nOctaveLayers
+		0.04,	// contrastThreshold
+		10,		// edgeThreshold
+		1.6		// sigma
+	);
+
+	// Convert Frame to RGB
 	cvtColor(frame->getData(), frame->getData(), CV_BGR2RGB);
-	frame = calibratePosition(frame);
-	auto t2 = std::chrono::high_resolution_clock::now();
-	
-	return frame; //calibratePosition(frame);
+
+	// Morph Frame
+	calibratePosition();
+	//calibrateLens();
+
+	return frame;
 }
 
 /*
 	calibrateLens
 */
-void CalibrationProcessor::calibrateLens(std::shared_ptr<Frame> frame) {
+void CalibrationProcessor::calibrateLens() {
+	std::shared_ptr<CameraDevice> camera = CameraDevice::devices[frame->getCameraID()];
 
+	std::cout << camera->getOpenCVProperty(CV_CAP_PROP_CONTRAST) << "\n";
+	camera->setOpenCVProperty(CV_CAP_PROP_CONTRAST, 30);
 }
 
 /*
 	calibratePosition
 */
-std::shared_ptr<Frame> CalibrationProcessor::calibratePosition(std::shared_ptr<Frame> frame) {
-	std::vector<cv::KeyPoint> keypoints;
-	cv::Mat output;
-
-	auto t1 = std::chrono::high_resolution_clock::now();
-	sift_detector->detect(frame->getData(), keypoints);
-	auto t2 = std::chrono::high_resolution_clock::now();
-	std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() << "\n";
-
+void CalibrationProcessor::calibratePosition() {
+	std::vector<cv::KeyPoint> keypoints = getKeypoints();
 
 	cv::drawKeypoints(frame->getData(), keypoints, frame->getData());
-
-	//std::cout << frame->getCameraID() << "\n";
-
-	return frame;
 }
 
-void CalibrationProcessor::calibrateCameraValue(int camera_id, int opencv_param_id, double opencv_param_value) {
-	std::shared_ptr<CameraDevice> camera = CameraDevice::devices[camera_id];
-	camera->setOpenCVProperty(opencv_param_id, opencv_param_value);
+/*
+ *
+ *	Helper Functions
+ *
+ */
+
+std::vector<cv::KeyPoint> CalibrationProcessor::getKeypoints() {
+	std::vector<cv::KeyPoint> keypoints;
+	sift_detector->detect(frame->getData(), keypoints);
+
+	filterKeypoints(keypoints);
+
+	return historic_features;
+}
+
+void CalibrationProcessor::filterKeypoints(std::vector<cv::KeyPoint> kp) {
+	if (frame->getID() % 10 == 0) {
+		std::cout << historic_features.size() << "\n";
+		historic_features.pop_back();
+	}
+
+	else {
+		for (cv::KeyPoint keypoint : kp) {
+			if (historic_features.size() < 10) {
+				historic_features.push_back(keypoint);
+			}
+		}
+	}
 }
