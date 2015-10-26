76020269cd404483a44de5008d92db68538c20e2
Alex Jaeger <agjaeger@ualr.edu>
2015-10-26 17:19:57 -0500

    Major Frame update
    actually minor but FrameID's are always unique
    Added ==operator override to compare frames

--- src/Frame.cpp
+++ src/Frame.cpp
@@ -1,27 +1,23 @@
 #include "Frame.h"
-#include <chrono>
-//#include "vld.h"
 
 Frame::Frame() {
 	_camera_id = -1;
-	_frame_id = -1;
-} 
+	_frame_id = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+}
 
 Frame::Frame(cv::Mat source, int camera_id) {
 	_frame = source;
-	//_frame = *source;
-	this->_camera_id = camera_id;
-	this->_frame_id = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-	//printf("Frame id: %llu \n",_frame_id);
+
+	_camera_id = camera_id;
+	_frame_id = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 }
 
-Frame::Frame(cv::Mat source,int camera_id, unsigned long long frame_id) {
+Frame::Frame(cv::Mat source, int camera_id, unsigned long long frame_id) {
 	_frame = source;
-	//_frame = *source;
-	this->_camera_id = camera_id;
-	this->_frame_id = frame_id;
-}
 
+	_camera_id = camera_id;
+	_frame_id = frame_id;
+}
 
 cv::Mat Frame::getData() {
 	return _frame;
@@ -33,4 +29,12 @@ int Frame::getCameraID() {
 
 unsigned long long Frame::getID() {
 	return _frame_id;
+}
+
+bool Frame::operator==(Frame f1) {
+	if (f1.getID() == _frame_id) {
+		return true;
+	}
+
+	return false;
 }
