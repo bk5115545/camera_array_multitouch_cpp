f03d934ec29f85c4db076f5f62c660f749c6a3a0
Alex Jaeger <agjaeger@ualr.edu>
2015-12-21 14:37:52 -0600

    Fixed DBScan Processing
    Now DBScan *correctly* identifies clusters
    
    BUG
    DBScan will sometimes create clusters instead of appending to pre
    existing, I am looking in to this problem

--- src/DBScanProcessor.cpp
+++ src/DBScanProcessor.cpp
@@ -14,19 +14,20 @@ DBScanProcessor::DBScanProcessor() {
 }
 
 std::shared_ptr<Frame> DBScanProcessor::computeFrame(std::shared_ptr<Frame> current_frame) {
-	DBPoint current_point;
 	std::vector<Cluster> clusters;
 	
 	motion_mat = boost::any_cast<cv::Mat>(current_frame->getFeature("motion_mask"));
 	motion_locations = boost::any_cast<cv::Mat>(current_frame->getFeature("motion_locs"));
 
 	for (int i = 0; i < motion_locations.rows; i++) {
+		DBPoint current_point;
+
 		current_point.pt = motion_locations.at<cv::Point>(i);
 
 		if (!current_point.visited) {
 			current_point.visited = true;
 
-			Cluster current_neighbors (100);
+			Cluster current_neighbors(100);
 			getRegion(current_point, current_neighbors);
 
 			if (current_neighbors.size() >= minPoints)
@@ -46,7 +47,20 @@ void DBScanProcessor::getRegion(DBPoint loc, Cluster & neighbors) {
 	DBPoint current_point;
 
 	neighbors.push_back(loc);
-	
+
+	for (int i = min_x; i < (min_x + maxDist); i++) {
+		for (int j = min_y; j < (min_y + maxDist); j++) {
+			current_point.pt = cv::Point(i, j);
+
+			if (bounding_rect.contains(current_point.pt)) {
+				if (motion_mat.at<uchar>(current_point.pt) == 255) {
+					neighbors.push_back(current_point);
+				}
+			}
+		}
+	}
+
+/*
 	cv::Rect bounding_search_box(cv::Point(min_x, min_y), cv::Point(min_x + maxDist, min_y + maxDist));
 
 	for (int i = 0; i < motion_locations.rows; i++) {
@@ -56,52 +70,6 @@ void DBScanProcessor::getRegion(DBPoint loc, Cluster & neighbors) {
 			neighbors.push_back(current_point);
 	}
 
-	/*
-	for (int i = min_x; i < (min_x + maxDist); i++) {
-		for (int j = min_y; j < (min_y + maxDist); j++) {
-			current_point.pt = cv::Point(loc.pt.x + i, loc.pt.y + j);
-
-			if (bounding_rect.contains(current_point.pt))
-				if (motion_mat.at<uchar>(current_point.pt) == 255)
-					neighbors.push_back(current_point);
-		}
-	}*/
-
-	/*
-	cv::Mat region (cv::Size(maxDist, maxDist), motion_mat.type());
-	
-	cv::Mat nonZeroLocs;
-	cv::Point roi_loc;
-	cv::Point transformed;
-
-	float midPt = maxDist / 2;
-
-	cv::Mat temp (motion_mat.size(), motion_mat.type());;
-
-	cv::getRectSubPix(motion_mat, region.size(), loc.pt, region);
-	cv::findNonZero(region, nonZeroLocs);
-
-	for (int i = 0; i < nonZeroLocs.rows; i++) {
-		cv::Point r_loc = nonZeroLocs.at<cv::Point>(i);
-		cv::Point a (loc.pt.x + (midPt - r_loc.x), loc.pt.y + (midPt - r_loc.y));
-
-		temp.at<cv::Vec3b>(r_loc) = cv::Vec3b(0, 0, 0);
-	}
-
-	return temp;
-	*/
-	/*
-	for (int i = 0; i < region.rows; i++) {
-		for (int j = 0; j < region.cols; j++) {
-			cv::Vec3b color = region.at<cv::Vec3b>(cv::Point(j, i));
-
-			std::cout << (int)color[0] << (int)color[1] << (int)color[2] << std::endl;
-		}
-	}
-	*/
-	//cv::findNonZero(region, nonZeroLocs);
-
-/*
 	neighbors.push_back(loc);
 	for (int i = 0; i < nonZeroLocs.rows; i++) {
 		roi_loc = nonZeroLocs.at<cv::Point>(i);
