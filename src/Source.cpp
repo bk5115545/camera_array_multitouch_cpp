bfa3cfcbbd298de31ce943aef77d16482617109d
Alex Jaeger <agjaeger@ualr.edu>
2015-12-21 14:36:50 -0600

    FIXED Image processing through Processors
    I fixed the pipeline to accept these images, I believe the bug was only
    due to image acquisition but now it works

--- src/Source.cpp
+++ src/Source.cpp
@@ -1,6 +1,10 @@
 
 #include <opencv2/opencv.hpp>
 
+#include <chrono>
+#include <iostream>
+#include <fstream>
+
 #include "CameraDevice.h"
 #include "Transformer.h"
 #include "Frame.h"
@@ -12,24 +16,29 @@
 #include "ClusterColorizerProcessor.h"
 
 int main(int argc, char * argv[]) {
-	// We are using the second rabbits dataset.
 	cv::VideoCapture image_seq (
 		"D:\\Projects\\camera_array_multitouch_cpp\\resources\\Testsets\\FBMS\\rabbits02\\rabbits02_%4d.jpg"
 	);
-	
-	cv::VideoWriter vid_output (
-		"D:\\Projects\\camera_array_multitouch_cpp\\resources\\program-output\\output.avi", static_cast<int>(image_seq.get(CV_CAP_PROP_FOURCC)), 
-		image_seq.get(CV_CAP_PROP_FPS), cv::Size((int) image_seq.get(CV_CAP_PROP_FRAME_WIDTH), (int) image_seq.get(CV_CAP_PROP_FRAME_HEIGHT)), true
-	);
+
+	std::ofstream log;
+	log.open("D:\\Projects\\camera_array_multitouch_cpp\\resources\\program-output\\log.txt");
 
 	Transformer main_chain;
+	
 	main_chain.addProcessor(new MotionProcessor());
 	main_chain.addProcessor(new DBScanProcessor());
 	main_chain.addProcessor(new ClusterColorizerProcessor());
-
+	
 	cv::Mat image;
+	int image_id = 0;
+
+	std::vector<std::chrono::milliseconds> frame_times;
+
+	auto total_start_time = std::chrono::steady_clock::now();
 
 	while (true) {
+		auto frame_start_time = std::chrono::steady_clock::now();
+
 		image_seq >> image;
 
 		if (image.empty())
@@ -40,13 +49,38 @@ int main(int argc, char * argv[]) {
 		frame = main_chain.getResult();
 		
 		image = boost::any_cast<cv::Mat>(frame->getFeature("color_clusters"));
-
+		
+		auto frame_end_time = std::chrono::steady_clock::now();
+		
 		cv::imshow("blah", image);
+		cv::imwrite("D:\\Projects\\camera_array_multitouch_cpp\\resources\\program-output\\output_" + std::to_string(image_id) + ".png", image);
+
 		cv::waitKey(5);
+		image_id++;
+
+		auto duration = std::chrono::duration_cast<std::chrono::milliseconds> (frame_end_time - frame_start_time);
+		frame_times.push_back(duration);
+		log << "Frame Processed in " << duration.count() << " ms" << std::endl;
 	}
 
+	auto total_end_time = std::chrono::steady_clock::now();
+
+	auto duration = std::chrono::duration_cast<std::chrono::seconds> (total_end_time - total_start_time);
+	log << "All Frames Processed in " << duration.count() << " s" << std::endl;
+
+	double average = 0.0;
+
+	for (auto i : frame_times) {
+		average += i.count();
+	}
+
+	average /= frame_times.size();
+
+	log << "Average Frame time " << average << " ms" << std::endl;
+
+	log.close();
+
 	image_seq.release();
-	vid_output.release();
 
 	return 0;
 }
